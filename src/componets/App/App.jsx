ce04192cb2e7c5a17ed70b348751562c2b765ace
Bloom <bloomillus@gmail.com>
2024-05-13 19:18:32 -0500

    first commit

---
+++ src/componets/App/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-zinc-300'>
+        Holis
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import App from './componets/App/App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
