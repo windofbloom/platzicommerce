5642e19a58e5f4cb7e378aa7ba9eb6bceadf8cb5
Bloom <bloomillus@gmail.com>
2024-05-13 19:45:50 -0500

    Added pages to App.jsx

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -2,6 +2,11 @@ import './App.css'
 
 /* PAGES */
 import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SignIn from '../SignIn'
 
 
 function App() {
@@ -9,6 +14,11 @@ function App() {
     <>
       <div className='bg-zinc-300'>
         <Home />
+        <MyAccount />
+        <MyOrder />
+        <MyOrders />
+        <NotFound />
+        <SignIn />
       </div>
 
     </>
