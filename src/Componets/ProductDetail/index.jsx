0eca7c66f5bceb15ffbb0fa4de56c6038513fbe3
Bloom <bloomillus@gmail.com>
2024-05-21 21:03:37 -0500

    Added basic style to pop up with product information

--- src/Componets/ProductDetail/index.jsx
+++ src/Componets/ProductDetail/index.jsx
@@ -1,7 +1,7 @@
 function ProductDetail () {
     return (
         <section className="flex flex-col fixed rounded-lg bg-white border border-black">
-
+            <h1>Hola</h1>
         </section>
     )
 }
