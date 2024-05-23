7b6e0c74c3a92dfbc7981ba95acd0179fd309235
Bloom <bloomillus@gmail.com>
2024-05-22 20:55:22 -0500

    Fixed event propagation in card and add fucition to x button to close the product pop up

--- src/Componets/Card/index.jsx
+++ src/Componets/Card/index.jsx
@@ -15,8 +15,7 @@ function Card (data) {
                  text-black text-xs m-2 px-3 py-0.5 ">{data.data.category}</div>
                 <img className="w-full h-full object-cover rounded-lg " src={data.data.image} alt={data.data.title} />
                 <button className="absolute top-0 right-0 flex justify-center items-center
-                bg-white w-6 h-6 rounded-full m-2 p-1"
-                    onClick={()=> context.setCount(context.count + 1)}> 
+                bg-white w-6 h-6 rounded-full m-2 p-1"> 
                 <IconPlus /></button>
             </figure>
             <p className="flex justify-between">
@@ -26,7 +25,7 @@ function Card (data) {
             <button className="flex justify-between"
                 onClick={(e)=> {
                     e.stopPropagation();
-                    context.setCount(context.count + 1)}>
+                    context.setCount(context.count + 1)}}>
                     <IconShoppingBag /> Add to Cart 
             </button>
         </article>
--- src/Componets/ProductDetail/index.jsx
+++ src/Componets/ProductDetail/index.jsx
@@ -1,9 +1,11 @@
-import { IconX } from '@tabler/icons-react';
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context';
 
+import { IconX } from '@tabler/icons-react';
+
 function ProductDetail () {
     const context = useContext(ShoppingCartContext);
+
 
     return (
         <section 
@@ -12,7 +14,8 @@ function ProductDetail () {
                  bg-white border border-black w-[calc(70vh-90px)]`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Product Details</h2>
-                <IconX />
+                <IconX className='cursor-pointer'
+                onClick={() => context.closeProductDetial()}/>
             </div>
         </section>
     )
