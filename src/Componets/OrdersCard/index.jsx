3be9d13f11c7aadfaaf34fbacae4d70ef6bda469
Bloom <bloomillus@gmail.com>
2024-05-29 21:59:58 -0500

    Fixed title basic style

--- src/Componets/OrdersCard/index.jsx
+++ src/Componets/OrdersCard/index.jsx
@@ -9,14 +9,14 @@ function OrdersCard (props) {
         <div className="flex justify-between items-center mb-3 
         border border-black p-4 rounded-lg w-80">
             <div className='flex justify-between w-full'>
-                <div className='flex flex-col'>
+                <p className='flex flex-col'>
                 <span className='flex justify-between items-center gap-2 mb-2'><IconCalendar /> 01.02.23</span>
                 <span className='font-light flex justify-between items-center gap-2'><IconShoppingCart /> {totalProducts} articles</span>
-                </div>
-                <span className='font-medium text-2xl flex justify-between
+                </p>
+                <p className='font-medium text-2xl flex justify-between
                     items-center gap-2'>${totalPrice}
                     <IconChevronsRight />
-                </span>
+                </p>
             </div>
         </div>
     )
