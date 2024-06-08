edbad1357125a96d4d314710ab9d9e4a0bca13d0
Bloom <bloomillus@gmail.com>
2024-06-07 21:14:17 -0500

    Fixing issue with header

---
+++ src/Componets/Header/index.jsx
@@ -0,0 +1,20 @@
+import ImageHeader from "../../utils/ImageHeader"
+
+function Header () {
+    return (
+        <div className=" flex flex-col lg:flex-row items-center justify-between 
+        p-4 space-y-4 lg:space-y-0 lg:space-x-8">
+         <div className="max-w-md text-center lg:text-left">
+             <h1 className="text-6xl font-bold mb-2 font-serif">Shopi Store,</h1>
+             <p className="mb-2 text-3xl font-mono">Crafted to blend fashion with function</p>
+            <h2 className="text-xl font-mono">Ecommerce design made with ♥ by <a href="https://github.com/windofbloom" className="hover:underline font-bold">windofbloom</a></h2>
+        </div>
+         <div>
+            <ImageHeader />
+        </div>
+</div>
+
+    )
+}
+
+export default Header
